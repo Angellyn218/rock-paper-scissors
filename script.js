63fb262655373287dd86fbb23bd4c416e4463124
Angellyn218 <angellyncervantes@yahoo.com>
2022-09-28 13:17:19 -0700

    Added stats confirm pop up

--- script.js
+++ script.js
@@ -46,6 +46,8 @@ function computerDecision() {
         alert("You lost!");
         stats.loses = stats.loses + 1;
     }
+    
+    confirm ("Current stats \n \n    Wins\: " + stats.wins + "\n    Loses\: " + stats.loses + "\n    Ties: " + stats.ties + "\n \n Would you like to play again?");
 
 
     
